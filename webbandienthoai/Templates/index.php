515bb7ff47b99d88c7df2e19c9a5b57f73066a3d
DESKTOP-J8FE15F\Cuong Vu <anhcuonghd1996@gmail.com>
2017-05-15 00:43:52 +0700

    version 1

---
+++ webbandienthoai/Templates/index.php
@@ -0,0 +1,125 @@
+<!doctype html PUBLIC "-//W3C/DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtmll/DTD/xhtmll-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<link rel="stylesheet" tyle="text/css" href="../style/css.css" />
+<title>Web bán điện thoại</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+<body>
+
+<div class="wrapper">
+	<div class=" header">
+		<img src="../images/banner.png" width="100%" height="120px" /;
+	</div>
+	
+	<div class="menu">
+		<ul>
+			<li><a href="#">Trang chủ</a></li>
+			<li><a href="#">Sản phẩm</a></li>
+			<li><a href="#">Hướng dẫn</a></li>
+			<li><a href="#">Liên hệ</a></li>			
+		</ul>
+	</div>
+	
+	<div class="content">
+		<div class="left">
+			<p style="text-align:center;color:red;background:#0CF;padding:10px;">Sản phẩm</p>
+			<div class="danhsachsanpham">
+				<ul>
+					<li><a href="#">Iphone</a></li>
+					<li><a href="#">Galaxy</a></li>
+					<li><a href="#">HTC</a></li>
+					<li><a href="#">ZenPhone</a></li>
+				</ul>
+			</div>
+			<p style="text-align: center;color:red;background:#0CF;padding:10px;">Hãng Sản phẩm</p>
+			<div class="danhsachsanpham">
+				<ul>
+					<li><a href="#">Apple</a></li>
+					<li><a href="#">SamSung</a></li>
+					<li><a href="#">HTC</a></li>
+					<li><a href="#">ASUS</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="right">
+		<p style="text-align: center;color:red;background:#0CF;padding:10px;">Tất Cả Sản phẩm</p>
+			<div class="sanphamall">
+				<ul>
+					<li><a href="#">
+						<img src="../images/iphone-6s.png" width="100" height="100"/>
+						<p>Iphone 6s</p>
+						<p style="color:#F00;">Giá: 6.000.000đ</p>
+						<p style="color:#f00; text-align: center">Chi Tiết</p>
+					</a></li>
+					<li><a href="#">
+						<img src="../images/samsungs6.jpg" width "100" height="100"/>
+						<p>Galaxy S6</p>
+						<p style="color:#F00;">Giá: 4.500.000đ</p>
+						<p style="color:#f00; text-align: center">Chi Tiết</p>
+					</a></li>
+					<li><a href="#">
+						<img src="../images/zenphon6.png" width="100" height="100"/>
+						<p>Zenphone 6</p>
+						<p style="color:#F00;">Giá: 4.500.000đ</p>
+						<p style="color:#f00; text-align: center">Chi Tiết</p>
+					</a></li>
+					<li><a href="#">
+						<img src="../images/iphone-6s.png" width="100" height="100"/>
+						<p>Iphone 6s</p>
+						<p style="color:#F00;">Giá: 6.000.000đ</p>
+						<p style="color:#f00; text-align: center">Chi Tiết</p>
+					</a></li>
+					<li><a href="#">
+						<img src="../images/iphone-5s.jpg" width="100" height="100"/>
+						<p>Iphone 5s</p>
+						<p style="color:#F00;">Giá:4.000.000đ</p>
+						<p style="color:#f00; text-align: center">Chi Tiết</p>
+					</a></li>
+					<li><a href="#">
+						<img src="../images/iPhone7Plus.png" width="100" height="100"/>
+						<p>Iphone 7s</p>
+						<p style="color:#F00;">Giá:19.000.000đ</p>
+						<p style="color:#f00; text-align: center">Chi Tiết</p>
+					</a></li>
+					<li><a href="#">
+						<img src="../images/samsungs7.jpg" width="100" height="100"/>
+						<p>Galaxy S7</p>
+						<p style="color:#F00;">Giá:17.000.000đ</p>
+						<p style="color:#f00; text-align: center">Chi Tiết</p>
+					</a></li>
+					<li><a href="#">
+							<img src="../images/htcOneA9.png" width="100" height="100"/>
+							<p>HTC One A9</p>
+							<p style="color:#F00;">Giá:5.490.000đ</p>
+							<p style="color:#f00; text-align: center">Chi Tiết</p>
+					</a></li>
+					<li><a href="#">
+							<img src="../images/samsungs8.jpeg" width="100" height="100"/>
+							<p>Galasy S8</p>
+							<p style="color:#F00;">Giá:21.490.000đ</p>
+							<p style="color:#f00; text-align: center">Chi Tiết</p>
+					</a></li>
+					<li><a href="#">
+							<img src="../images/zenphon5.jpg" width="100" height="100"/>
+							<p>Zenphone 5</p>
+							<p style="color:#F00;">Giá:4.000.000đ</p>
+							<p style="color:#f00; text-align: center">Chi Tiết</p>
+					</a></li>
+				</ul>
+			</div>
+		</div>
+	<div class="clear"></div>
+	<div class="footer">
+		<h3 style="color: #909;text-align: center;line-height: 80px">Copyright 2017</h3>
+	</div>
+	<div class="clear"></div>
+	
+</div> 
+</body>
+</html>
